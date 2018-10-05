501a3e9e6838a9f91fc94c0ad509ab42db0c5018
Tank <zxm55547@gmail.com>
2018-10-06 00:43:33 +0800

    完成，含測試

--- ReceiptLottery/ViewController.swift
+++ ReceiptLottery/ViewController.swift
@@ -16,11 +16,93 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var resultLabel: UILabel!
     
+    let numberData = ["特別獎":"73372972","特獎":"22315462","頭獎1":"91903003","頭獎2":"16228722","頭獎3":"03270598","普獎1":"163","普獎2":"983","普獎3":"814"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func confirmButtonAction(_ sender: Any) {
+        
+        if checkIsEightNumber(text: numberTextField.text) {
+            
+            resultLabel.text = receiptLottery(text: numberTextField.text!).rawValue
+            
+        }else {
+            resultLabel.text = "請輸入正確數字"
+        }
+    }
+    
+    enum Bonus : String {
+        case first = "中一千萬"
+        case second = "中兩百萬"
+        case three = "中二十萬"
+        case four = "中四萬"
+        case five = "中一萬"
+        case six = "中四千"
+        case seven = "中一千"
+        case eight = "中兩百"
+        case none = "沒有中獎"
+    }
+    
+    
+    func receiptLottery(text : String) -> Bonus {
+        
+        let lotteryString = text
+        
+        if lotteryString == numberData["特別獎"] {
+            return .first
+        }else if lotteryString == numberData["特獎"]{
+            return .second
+        }else if lotteryString == numberData["頭獎1"] ||  lotteryString == numberData["頭獎2"] ||  lotteryString == numberData["頭獎3"]{
+            return .three
+        }else {
+            for i in 1...5 {
+                let startIndex = lotteryString.index(lotteryString.startIndex, offsetBy: i)
+                let reversedNumber = lotteryString[startIndex..<lotteryString.endIndex]
+                print(reversedNumber)
+                let temp1 = numberData["頭獎1"]![startIndex..<lotteryString.endIndex]
+                let temp2 = numberData["頭獎2"]![startIndex..<lotteryString.endIndex]
+                let temp3 = numberData["頭獎3"]![startIndex..<lotteryString.endIndex]
+                if reversedNumber == temp1 || reversedNumber == temp2 || reversedNumber == temp3 {
+                    switch i{
+                    case 1:
+                        return .four
+                    case 2:
+                        return .five
+                    case 3:
+                        return .six
+                    case 4:
+                        return .seven
+                    case 5:
+                        return .eight
+                    default:
+                        break
+                    }
+                }
+            }
+            
+            let reversedNumber = String(lotteryString.reversed()[0...2])
+            let temp1 = String(numberData["普獎1"]!.reversed()[0...2])
+            let temp2 = String(numberData["普獎2"]!.reversed()[0...2])
+            let temp3 = String(numberData["普獎3"]!.reversed()[0...2])
+            if reversedNumber == temp1 || reversedNumber == temp2 || reversedNumber == temp3 {
+                return .eight
+            }
+        }
+        
+        return .none
+    }
+    
+    func checkIsEightNumber(text:String?) -> Bool {
+        
+        guard let numberString = text , numberString.count == 8 else {
+            return false
+        }
+        
+        //判斷是不是數字
+        return CharacterSet.decimalDigits.isSuperset(of: CharacterSet(charactersIn: numberString))
         
     }
     
--- ReceiptLotteryTests/ReceiptLotteryTests.swift
+++ ReceiptLotteryTests/ReceiptLotteryTests.swift
@@ -11,8 +11,13 @@ import XCTest
 
 class ReceiptLotteryTests: XCTestCase {
 
+    var vc: ViewController!
+
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        vc = storyboard.instantiateInitialViewController() as? ViewController
     }
 
     override func tearDown() {
@@ -30,5 +35,238 @@ class ReceiptLotteryTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
+    
+    func testErrorEnterNoNumberStringForCheckIsEightNumber(){
+        
+        let enterString = "aaaaaaaa"
+        
+        if vc.checkIsEightNumber(text: enterString) {
+            XCTAssert(false)
+        }else{
+            XCTAssert(true)
+        }
+    }
+    
+    func testErrorEnterLowEightNumberStringForCheckIsEightNumber(){
+        
+        let enterString = "1234567"
+        
+        if vc.checkIsEightNumber(text: enterString) {
+            XCTAssert(false)
+        }else{
+            XCTAssert(true)
+        }
+    }
+    
+    func testErrorEnterUpEightNumberStringForCheckIsEightNumber(){
+        
+        let enterString = "123456789"
+        
+        if vc.checkIsEightNumber(text: enterString) {
+            XCTAssert(false)
+        }else{
+            XCTAssert(true)
+        }
+    }
+    
+    func testEnterEightNumberStringForCheckIsEightNumber(){
+        
+        let enterString = "02345678"
+        
+        if vc.checkIsEightNumber(text: enterString) {
+            XCTAssert(true)
+        }else{
+            XCTAssert(false)
+        }
+    }
+    
+    
+    func testFirstOKReceiptLottery(){
+        
+        let enterString = "73372972"
+        if vc.receiptLottery(text: enterString) == .first {
+            XCTAssert(true)
+        }else{
+            XCTAssert(false)
+        }
+    }
+    
+    func testFirstFailReceiptLottery(){
+        
+        let enterString = "73372971"
+        if vc.receiptLottery(text: enterString) == .first {
+            XCTAssert(false)
+        }else{
+            XCTAssert(true)
+        }
+    }
+    
+    func testSecondOKReceiptLottery(){
+        
+        let enterString = "22315462"
+        if vc.receiptLottery(text: enterString) == .second {
+            XCTAssert(true)
+        }else{
+            XCTAssert(false)
+        }
+    }
+    
+    func testSecondFailReceiptLottery(){
+        
+        let enterString = "22315460"
+        if vc.receiptLottery(text: enterString) == .second {
+            XCTAssert(false)
+        }else{
+            XCTAssert(true)
+        }
+    }
+    
+  
+    func testThreeOKReceiptLottery(){
+        
+        let enterString = "91903003"
+        if vc.receiptLottery(text: enterString) == .three {
+            XCTAssert(true)
+        }else{
+            XCTAssert(false)
+        }
+    }
+    
+    func testThreeFailReceiptLottery(){
+        
+        let enterString = "91903000"
+        if vc.receiptLottery(text: enterString) == .three {
+            XCTAssert(false)
+        }else{
+            XCTAssert(true)
+        }
+    }
+    
+    
+    func testFourOKReceiptLottery(){
+        
+        let enterString = "96228722"
+        if vc.receiptLottery(text: enterString) == .four {
+            XCTAssert(true)
+        }else{
+            XCTAssert(false)
+        }
+    }
+    
+    func testFourFailReceiptLottery(){
+        
+        let enterString = "96228721"
+        if vc.receiptLottery(text: enterString) == .four {
+            XCTAssert(false)
+        }else{
+            XCTAssert(true)
+        }
+    }
+    
+    func testFiveOKReceiptLottery(){
+        
+        let enterString = "99228722"
+        if vc.receiptLottery(text: enterString) == .five {
+            XCTAssert(true)
+        }else{
+            XCTAssert(false)
+        }
+    }
+    
+    func testFiveFailReceiptLottery(){
+        
+        let enterString = "99228721"
+        if vc.receiptLottery(text: enterString) == .five {
+            XCTAssert(false)
+        }else{
+            XCTAssert(true)
+        }
+    }
+    
+    
+    func testSixOKReceiptLottery(){
+        
+        let enterString = "99928722"
+        if vc.receiptLottery(text: enterString) == .six {
+            XCTAssert(true)
+        }else{
+            XCTAssert(false)
+        }
+    }
+    
+    func testSixFailReceiptLottery(){
+        
+        let enterString = "99928721"
+        if vc.receiptLottery(text: enterString) == .six {
+            XCTAssert(false)
+        }else{
+            XCTAssert(true)
+        }
+    }
+    
+    
+    func testSevenOKReceiptLottery(){
+        
+        let enterString = "99998722"
+        if vc.receiptLottery(text: enterString) == .seven {
+            XCTAssert(true)
+        }else{
+            XCTAssert(false)
+        }
+    }
+    
+    func testSevenFailReceiptLottery(){
+        
+        let enterString = "99998721"
+        if vc.receiptLottery(text: enterString) == .seven {
+            XCTAssert(false)
+        }else{
+            XCTAssert(true)
+        }
+    }
+    
+    
+    func testEightOKReceiptLottery(){
+        
+        let enterString = "99999722"
+        if vc.receiptLottery(text: enterString) == .eight {
+            XCTAssert(true)
+        }else{
+            XCTAssert(false)
+        }
+    }
+    
+    func testEightFailReceiptLottery(){
+        
+        let enterString = "99999721"
+        if vc.receiptLottery(text: enterString) == .eight {
+            XCTAssert(false)
+        }else{
+            XCTAssert(true)
+        }
+    }
+    
+    
+    func testEight2OKReceiptLottery(){
+        
+        let enterString = "99999983"
+        if vc.receiptLottery(text: enterString) == .eight {
+            XCTAssert(true)
+        }else{
+            XCTAssert(false)
+        }
+    }
+    
+    func testEight2FailReceiptLottery(){
+        
+        let enterString = "99999981"
+        if vc.receiptLottery(text: enterString) == .eight {
+            XCTAssert(false)
+        }else{
+            XCTAssert(true)
+        }
+    }
+    
+    
 }
